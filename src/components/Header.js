9025a330979e6a7d87b5f438d57cdef5f2da2052
m13kelore <kelceylorenzo@gmail.com>
2018-03-19 12:34:38 -0700

    completed building and designing intro and cardpack selection page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ export default (props) => {
 		<div className="header">
 			<h2 className="main-title">Cards & Co.</h2>
 			<div class="link-bar settings">SETTINGS</div>
+			<div class="link-bar rules">RULES</div>
 			<div class="link-bar about">ABOUT</div>
 		</div>
 	);
--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Route, Redirect, Switch } from 'react-router';
 import IntroPage from './Intro';
+import About from './About';
+import CardSelection from './CardSelection';
+import backgroundImage from '../assets/images/Cappadocia-Desktop.png';
 import '../assets/css/app.css';
 
 export default () => {
-	return <IntroPage />;
+	const pageStyle = {
+		backgroundImage: `url('${backgroundImage}')`,
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		backgroundPosition: 'center'
+	};
+	return (
+		<div style={pageStyle}>
+			<Switch>
+				<Route exact path="/" component={IntroPage} />
+				<Route path="/about" component={About} />
+				<Route path="/start-game" component={CardSelection} />
+				<Redirect to="/" />
+			</Switch>
+		</div>
+	);
 };
