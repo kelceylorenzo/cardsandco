29fa3eabe16b69d5a38e58c7489d4674e58e24f9
m13kelore <kelceylorenzo@gmail.com>
2018-03-26 12:34:55 -0700

    basic fades between routes completed; fix bug on about page

--- src/components/CardSelection.js
+++ src/components/CardSelection.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 import { selectCardPack } from '../actions';
 import anchor from '../assets/images/cardpack-anchor.png';
 import balloon from '../assets/images/cardpack-balloon.png';
 import butterfly from '../assets/images/cardpack-butterfly.png';
 import shield from '../assets/images/cardpack-shield.png';
+import backgroundImage from '../assets/images/Cappadocia-Desktop.png';
 
 class CardSelection extends Component {
 	constructor(props) {
@@ -17,7 +18,7 @@ class CardSelection extends Component {
 		if (this.canBeClicked) {
 			this.canBeClicked = false;
 			this.props.selectCardPack(selection);
-			setTimeout(this.redirectToGamePage.bind(this), 1000);
+			this.redirectToGamePage();
 		}
 
 		return;
@@ -28,41 +29,41 @@ class CardSelection extends Component {
 	}
 
 	render() {
-		const transitionOptions = {
-			classNames: 'fade',
-			timeout: { enter: 500, exit: 500 }
+		const pageStyle = {
+			backgroundImage: `url('${backgroundImage}')`,
+			backgroundSize: 'cover',
+			backgroundRepeat: 'no-repeat',
+			backgroundPosition: 'center'
 		};
 		return (
-			<TransitionGroup component="div" className="fade">
-				<CSSTransition {...transitionOptions}>
-					<div className="page-container cardpack-selection-page">
-						<div className="cardpack-selection-title">select your card pack</div>
+			<div style={pageStyle}>
+				<div className="page-container cardpack-selection-page">
+					<div className="cardpack-selection-title">select your card pack</div>
 
-						<div className="cardpack-selectors">
-							<img
-								className="cardpack"
-								src={anchor}
-								onClick={() => this.handleCardPackSelection('anchor')}
-							/>
-							<img
-								className="cardpack"
-								src={balloon}
-								onClick={() => this.handleCardPackSelection('balloon')}
-							/>
-							<img
-								className="cardpack"
-								src={butterfly}
-								onClick={() => this.handleCardPackSelection('butterfly')}
-							/>
-							<img
-								className="cardpack"
-								src={shield}
-								onClick={() => this.handleCardPackSelection('shield')}
-							/>
-						</div>
+					<div className="cardpack-selectors">
+						<img
+							className="cardpack"
+							src={anchor}
+							onClick={() => this.handleCardPackSelection('anchor')}
+						/>
+						<img
+							className="cardpack"
+							src={balloon}
+							onClick={() => this.handleCardPackSelection('balloon')}
+						/>
+						<img
+							className="cardpack"
+							src={butterfly}
+							onClick={() => this.handleCardPackSelection('butterfly')}
+						/>
+						<img
+							className="cardpack"
+							src={shield}
+							onClick={() => this.handleCardPackSelection('shield')}
+						/>
 					</div>
-				</CSSTransition>
-			</TransitionGroup>
+				</div>
+			</div>
 		);
 	}
 }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import Header from './Header';
 import GameArea from './GameArea';
 import StatsContainer from './StatsContainer';
